dc271a4821b14be70233a49eefba668d2b85a980
Rémi <remi.choffat@gmail.com>
2025-01-16 11:43:32 +0100

    Ajout de l'utilisation de variables

--- mbash.c
+++ mbash.c
@@ -7,7 +7,6 @@
 #include <linux/limits.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <dirent.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
@@ -16,6 +15,18 @@ static pid_t child_pid = -1;
 
 // Longueur maximale d'une commande
 #define MAX_COMMAND_LENGTH 1024
+
+
+// Structure pour stocker les variables
+typedef struct
+{
+    char name[50];
+    char value[50];
+} Variable;
+
+Variable variables[100];
+int variable_count = 0;
+
 
 /**
  * Gestionnaire de signal SIGINT
@@ -71,6 +82,68 @@ char* get_prompt(char* envp[])
     }
 
     return prompt;
+}
+
+
+void set_variable(const char* name, const char* value)
+{
+    for (int i = 0; i < variable_count; i++)
+    {
+        if (strcmp(variables[i].name, name) == 0)
+        {
+            strcpy(variables[i].value, value);
+            return;
+        }
+    }
+    strcpy(variables[variable_count].name, name);
+    strcpy(variables[variable_count].value, value);
+    variable_count++;
+}
+
+char* get_variable(const char* name)
+{
+    for (int i = 0; i < variable_count; i++)
+    {
+        if (strcmp(variables[i].name, name) == 0)
+        {
+            return variables[i].value;
+        }
+    }
+    return NULL;
+}
+
+char* replace_variables(char* input)
+{
+    static char buffer[1024];
+    char* p = buffer;
+    while (*input)
+    {
+        if (*input == '$')
+        {
+            input++;
+            char var_name[50];
+            char* v = var_name;
+            while (isalnum(*input) || *input == '_')
+            {
+                *v++ = *input++;
+            }
+            *v = '\0';
+            char* value = get_variable(var_name);
+            if (value)
+            {
+                while (*value)
+                {
+                    *p++ = *value++;
+                }
+            }
+        }
+        else
+        {
+            *p++ = *input++;
+        }
+    }
+    *p = '\0';
+    return buffer;
 }
 
 
@@ -158,35 +231,71 @@ int eval(const char* expr)
     return atoi(result);
 }
 
+
 /**
  * Remplace les variables et évalue les expressions dans l'input donné.
  */
-char* replace_variables(char* input)
+/**
+ * Replaces variables and evaluates expressions in the given input.
+ */
+char* interprete_variables(char* input)
 {
     static char buffer[1024];
-
-    if (input[0] == '$')
+    char* p = buffer;
+    while (*input)
     {
-        if (input[1] == '(' && input[2] == '(')
+        if (*input == '$')
         {
-            char* expr = input + 3;
-            char* end = strstr(expr, "))");
-            if (end)
+            input++;
+            if (*input == '(' && *(input + 1) == '(')
             {
-                *end = '\0';
-                snprintf(buffer, sizeof(buffer), "%d", eval(expr));
-                return buffer;
+                input += 2;
+                char expr[100];
+                char* e = expr;
+                while (*input && !(*input == ')' && *(input + 1) == ')'))
+                {
+                    *e++ = *input++;
+                }
+                *e = '\0';
+                input += 2; // Skip the closing ))
+                snprintf(p, sizeof(buffer) - (p - buffer), "%d", eval(expr));
+                p += strlen(p);
+            }
+            else
+            {
+                char var_name[50];
+                char* v = var_name;
+                while (isalnum(*input) || *input == '_')
+                {
+                    *v++ = *input++;
+                }
+                *v = '\0';
+                char* value = get_variable(var_name);
+                if (value)
+                {
+                    strcpy(p, value);
+                    p += strlen(value);
+                }
+                else
+                {
+                    char* env_value = getenv(var_name);
+                    if (env_value)
+                    {
+                        strcpy(p, env_value);
+                        p += strlen(env_value);
+                    }
+                }
             }
         }
         else
         {
-            char* var_name = input + 1;
-            char* env_value = getenv(var_name);
-            return env_value ? env_value : "";
+            *p++ = *input++;
         }
     }
-    return input;
+    *p = '\0';
+    return buffer;
 }
+
 
 /**
  * Récupère le chemin complet vers le fichier .mbash_history dans le répertoire personnel.
@@ -212,6 +321,7 @@ int get_history_file_path(char* buffer, size_t size)
 
     return 0;
 }
+
 
 /**
  * Ajoute une commande dans le fichier d'historique.
@@ -300,7 +410,7 @@ int echo_command(char** args)
 {
     for (int i = 1; args[i]; i++)
     {
-        char* arg = replace_variables(args[i]);
+        char* arg = interprete_variables(args[i]);
         for (int k = 0; arg[k]; k++)
         {
             if (arg[k] == '\\' && arg[k + 1])
@@ -413,6 +523,17 @@ void execute_command(const char* command, char* args[], char* envp[])
  */
 int exec_command(char* command, char** args, char* envp[])
 {
+    if (strchr(command, '='))
+    {
+        char* name = strtok(command, "=");
+        char* value = strtok(NULL, "=");
+        if (name && value)
+        {
+            set_variable(name, value);
+        }
+        return 0;
+    }
+
     //cherche commande interne
     for (int i = 0; commands[i].command; i++)
     {
@@ -492,7 +613,9 @@ int main(int argc, char* argv[], char* envp[])
         char* args[1024];
         int i = 0;
         args[i++] = command;
-        while ((args[i++] = strtok(NULL, " ")));
+        while ((args[i++] = strtok(NULL, " ")))
+        {
+        }
 
         exec_command(command, args, envp);
 
